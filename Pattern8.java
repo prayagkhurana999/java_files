b10531cad9dfe4b0e861894fdc07f42a0a2407fe
prayag khurana <prayagkhurana999@gmail.com>
2020-10-09 22:38:52 +0530

    Create Pattern8.java

---
+++ Pattern8.java
@@ -0,0 +1,21 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    for(int i=1;i<=4;i++)
+	    {
+	        for(int j=1;j<=i;j++)
+	        {
+	            System.out.print(" ");
+	        }
+	        for(int k=i;k<=4;k++)
+	        {
+	            System.out.print("*");
+	        }
+	        for(int l=i;l<=4;l++)
+	        {
+	            System.out.print("*");
+	        }
+	        System.out.println();
+	    }
+	}
+}

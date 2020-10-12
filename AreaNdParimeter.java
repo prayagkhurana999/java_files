2892c1bd856e11a2039bdc2e120107ae8851f0ce
prayag khurana <prayagkhurana999@gmail.com>
2020-10-13 01:03:40 +0530

    pre questions

---
+++ AreaNdParimeter.java
@@ -0,0 +1,13 @@
+class areaandparimeter
+{
+	public static void main(String arg[])
+	{
+		int area=0,l=10,b=20,parimeter=0;
+		area=l*b;
+		parimeter=2*(l+b);
+		System.out.println("area="+area);
+		System.out.println("parimeter="+parimeter);
+
+
+	}
+}
